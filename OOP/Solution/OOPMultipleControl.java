dc177753288c7599a680fafbdba072d5c42ef24a
AMPC <amit.so100@gmail.com>
2017-12-26 11:19:33 +0200

    Started part1, not tested

--- OOP/Solution/OOPMultipleControl.java
+++ OOP/Solution/OOPMultipleControl.java
@@ -3,11 +3,31 @@ package OOP.Solution;
 import OOP.Provided.*;
 
 import java.io.File;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 public class OOPMultipleControl {
 
     private Class<?> interfaceClass;
     private File sourceFile;
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface OOPMultipleInterface { }
+
+    public enum OOPMethodModifier {
+        PUBLIC(2),
+        PROTECTED(1),
+        PRIVATE(0);
+    }
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface OOPMultipleMethod { 
+        OOPMethodModifier modifier() default PUBLIC;
+    }
 
     public OOPMultipleControl(Class<?> interfaceClass, File sourceFile) {
         this.interfaceClass = interfaceClass;
