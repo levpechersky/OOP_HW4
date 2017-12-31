dab8248b02ff882bcbe977bc8b579df077164107
Lev Pechersky <levpechersky@gmail.com>
2017-12-31 16:16:04 +0200

    refactor inherent ambiguities check, add comments, minor refactoring of other methods.

--- OOP/Solution/OOPMultipleControl.java
+++ OOP/Solution/OOPMultipleControl.java
@@ -115,73 +115,99 @@ public class OOPMultipleControl {
         }
     }
 
-    // TODO: refactor this
+    /**
+     * General idea: we want to simulate the way methods are inherited and overriden in C++.
+     * We traverse inheritance graph from most basic interfaces to most derived (reverse topological order), and
+     * for each class we keep track of methods it knows and which interface first declared that method (look
+     * InheritedMethod class).
+     * @throws OOPInherentAmbiguity
+     */
     private void checkGraphInherentAmbiguities() throws OOPInherentAmbiguity {
+
         Deque<Class<?>> topoSort = topologicalSort(this.interfaceClass);
 
+        /* For each interface in inheritance graph we keep list of methods it knows (either inherited or declared).
+        *
+        *  Note: values in this map are valid only for classes we already processed
+        *  (i.e. from current to the end of topological sort)
+        */
         Map<Class<?>, List<InheritedMethod>> inheritedAndDeclaredMethods = topoSort.stream()
                 .collect(Collectors.toMap(Function.identity(), unused -> new LinkedList<>()));
 
         Iterator i = topoSort.descendingIterator();
         while (i.hasNext()){
-            Class<?> current = (Class<?>) i.next();
-            // 1. get declared and inherited methods from direct parents
-            List<InheritedMethod> myMethods = inheritedAndDeclaredMethods.get(current);
-            for (Class<?> baseInterface : current.getInterfaces()){
-                myMethods.addAll(inheritedAndDeclaredMethods.get(baseInterface));
-            }
-            // 2. add current class methods, override existing (remove them from my methods)
-            // 2.1 remove inherited methods which I override
-            myMethods.removeIf(aMethod -> {
-                        try {
-                            current.getMethod(aMethod.method.getName(), aMethod.method.getParameterTypes());
-                            return false;
-                        } catch (NoSuchMethodException e) {
-                            return true;
-                        }
-                    }
-            );
-            // 2.2 add all methods overriden by me
-            myMethods.addAll(Arrays.stream(current.getDeclaredMethods())
-                    .map(aMethod -> new InheritedMethod(aMethod, current))
-                    .collect(Collectors.toList()));
-            // 3. check for ambiguities
-            for (InheritedMethod m : myMethods) {
-                final String mname = m.method.getName();
-                final Class<?>[] args = m.method.getParameterTypes();
-                long occurences = myMethods.stream()
-                        .filter(inMethod -> {
-                                    return inMethod.method.getName().equals(mname) &&
-                                            Arrays.equals(inMethod.method.getParameterTypes(), args);
-                                }
-                        ).count();
-                if (occurences>1) {
-                    throw new OOPInherentAmbiguity(this.interfaceClass, m.definingClass, m.method);
-                }
-            }
+            Class<?> currentInterface = (Class<?>) i.next();
+            List<InheritedMethod> myMethods = getMethodsFromBaseInterfaces(currentInterface, inheritedAndDeclaredMethods);
+            applyMethodsOverride(currentInterface, myMethods);
+            checkForAmbiguities(myMethods);
         }
     }
 
-    private class InheritedMethod {
-        public Method method;
-        public Class<?> definingClass;
-
-        public InheritedMethod(Method method, Class<?> definingClass){
-            this.method = method;
-            this.definingClass = definingClass;
+    private static List<InheritedMethod> getMethodsFromBaseInterfaces(Class<?> current, Map<Class<?>, List<InheritedMethod>> inheritedAndDeclaredMethods) {
+        List<InheritedMethod> myMethods = inheritedAndDeclaredMethods.get(current);
+        for (Class<?> baseInterface : current.getInterfaces()) {
+            myMethods.addAll(inheritedAndDeclaredMethods.get(baseInterface));
         }
+        return myMethods;
+    }
 
-        @Override
-        public int hashCode(){
-            return method.getName().hashCode() ^ definingClass.hashCode();
+    private void applyMethodsOverride(Class<?> currentInterface, List<InheritedMethod> inheritedMethods) {
+        // 1. remove inherited methods which currentInterface overrides
+        inheritedMethods.removeIf(aMethod -> {
+                    try {
+                        currentInterface.getMethod(aMethod.method.getName(), aMethod.method.getParameterTypes());
+                        return false;
+                    } catch (NoSuchMethodException e) {
+                        return true;
+                    }
+                });
+        // 2. add all methods overriden by me
+        inheritedMethods.addAll(Arrays.stream(currentInterface.getDeclaredMethods())
+                .map(aMethod -> new InheritedMethod(aMethod, currentInterface))
+                .collect(Collectors.toList()));
+    }
+
+    /**
+     * We simply look for duplicates in list of all methods an interface knows.
+     * Note: java's Method.equals() can't do this, since methods declared in different classes aren't equal.
+     *  Instead we compare only declarations.
+     * @param inheritedAndDeclaredMethods - all methods which an interface knows, after applying override.
+     * @throws OOPInherentAmbiguity
+     */
+    private void checkForAmbiguities(List<InheritedMethod> inheritedAndDeclaredMethods) throws OOPInherentAmbiguity {
+        for (InheritedMethod m : inheritedAndDeclaredMethods) {
+            final String mname = m.method.getName();
+            final Class<?>[] args = m.method.getParameterTypes();
+            long occurrences = inheritedAndDeclaredMethods.stream().filter(inMethod ->
+                    inMethod.method.getName().equals(mname) && Arrays.equals(inMethod.method.getParameterTypes(), args))
+                    .count();
+            if (occurrences > 1) {
+                throw new OOPInherentAmbiguity(this.interfaceClass, m.declaringClass, m.method);
+            }
         }
     }
 
     /**
-     *
+     * Helper class for inherent ambiguities check.
+     * That's how we keep track which interface declares each method.
+     */
+    private class InheritedMethod {
+        public Method method;
+        public Class<?> declaringClass;
+
+        public InheritedMethod(Method method, Class<?> declaringClass){
+            this.method = method;
+            this.declaringClass = declaringClass;
+        }
+    }
+
+    /**
+     * BFS traversal of inheritance graph.
      * @param start - start node.
      * @param callbackFunction - function to invoke for each node (look BFSCallBack).
      * @param callbackArgument - additional argument to pass to callbackFunction.
+     *                         Type is Object for the sake of genericity.
+     *                         It's callbackFunction responsibility to cast callbackArgument to an appropriate type.
      * @throws OOPMultipleException
      */
     private static void doBFS(Class<?> start, BFSCallBack callbackFunction, Object callbackArgument)
@@ -201,17 +227,18 @@ public class OOPMultipleControl {
         void invoke(Class<?> node, Object callbackArgument) throws OOPMultipleException;
     }
 
+
     private static Deque<Class<?>> topologicalSort(Class<?> start) {
-        Map<Class<?>, Integer> graph = getGraphWithNumberOfImplementors(start);
+        Map<Class<?>, Integer> graph = countInterfaceImplementors(start);
         Deque<Class<?>> sorted = new ArrayDeque<>();
         Deque<Class<?>> sources = new ArrayDeque<>();
 
         sources.add(start);
 
-        while (!sources.isEmpty()){
+        while (!sources.isEmpty()) {
             Class<?> current = sources.removeFirst();
             sorted.addLast(current);
-            for (Class<?> baseInterface : current.getInterfaces()){
+            for (Class<?> baseInterface : current.getInterfaces()) {
                 Integer count = graph.get(baseInterface);
                 graph.put(baseInterface, count - 1);
                 if (graph.get(baseInterface) == 0)
@@ -225,15 +252,15 @@ public class OOPMultipleControl {
      *
      * @return Map (Interface)->(number of interfaces which extend it)
      */
-    private static Map<Class<?>, Integer> getGraphWithNumberOfImplementors(Class<?> start){
+    private static Map<Class<?>, Integer> countInterfaceImplementors(Class<?> start) {
         Map<Class<?>, Integer> result = new HashMap<>();
         try {
-            doBFS(start, (aClass, accumulator)->{
+            doBFS(start, (aClass, accumulator) -> {
                 HashMap<Class<?>, Integer> map = (HashMap<Class<?>, Integer>) accumulator;
                 Integer count = map.getOrDefault(aClass, 0);
                 map.put(aClass, count + 1);
             }, result);
-        } catch (OOPMultipleException e){
+        } catch (OOPMultipleException e) {
             e.printStackTrace();
             assert false;
         }
